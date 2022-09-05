606899b6e35d9d10290bf0a057cdba3443df7dac
npxrc <npxrc@duck.com>
2022-09-04 21:25:21 -0500

    Update 404.js

--- assets/js/404.js
+++ assets/js/404.js
@@ -77,21 +77,6 @@ let nameList = [
   item.replace('...','')
   item.replace('','')
 document.getElementById('generateText').innerHTML = 'You misspelled /' + item + extension
-setTimeout(()=>{
+setTimeout(() => {
     document.getElementById('generateText').innerHTML = 'This page will redirect to the home page in 5 seconds.'
-},10000)
-setTimeout(()=>{
-    document.getElementById('generateText').innerHTML = 'This page will redirect to the home page in 4 seconds.'
-},1000);
-setTimeout(()=>{
-    document.getElementById('generateText').innerHTML = 'This page will redirect to the home page in 3 seconds.'
-},1000);
-setTimeout(()=>{
-    document.getElementById('generateText').innerHTML = 'This page will redirect to the home page in 2 seconds.'
-},1000);
-setTimeout(()=>{
-    document.getElementById('generateText').innerHTML = 'This page will redirect to the home page in 1 second.'
-},1000);
-setTimeout(()=>{
-    location.replace('https://thegamers.netlify.app')
-},1000)
+}, 10000);
